c245d67e19900b2128842e6b00266c39b61d89d4
Patrice Brend'amour <patrice@brendamour.net>
2021-08-10 21:09:46 +0200

    updated dependency

--- cli/Package.swift
+++ cli/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .executable(name: "changelog-generator", targets: ["changelog-generator"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
